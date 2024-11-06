edcff8277802bea82f0f6a3d1afbf4bd1fdf97a9
Dun2901 <lamchidung373@gmail.com>
2024-11-06 11:37:03 +0700

    project structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { useState } from "react";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import LoginPage from "./pages/login";
+import ContactPage from "./pages/contact";
+import BookPage from "./pages/book";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 const Layout = () => {
-  return <>main page</>;
+  return (
+    <div>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default function App() {
@@ -12,6 +23,18 @@ export default function App() {
       path: "/",
       element: <Layout />,
       errorElement: <div>404 not found</div>,
+
+      children: [
+        { index: true, element: <Home /> },
+        {
+          path: "contact",
+          element: <ContactPage />,
+        },
+        {
+          path: "book",
+          element: <BookPage />,
+        },
+      ],
     },
     {
       path: "/login",
